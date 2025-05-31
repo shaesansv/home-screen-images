1efca8ece901333710afc9545766f29994ad60f6
shaesansv <shaesan77@gmail.com>
2025-05-31 20:53:37 +0530

    Login Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,40 @@
-import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 import Background from "./components/Background/Background";
+import Login from "./components/Login/Login";
+import { useState } from "react";
 
 function App() {
+  const [heroCount, setHeroCount] = useState(0);
+
   let heroData = [
     { text1: "Dive into", text2: "what you love" },
     { text1: "Indulge", text2: "your passions" },
     { text1: "Give in to", text2: "your passions" },
   ];
-  const [heroCount, setHeroCount] = useState(0);
 
   return (
-    <div>
-      <Background heroCount={heroCount} />
-      <Navbar />
-      <Hero
-        heroData={heroData[heroCount]}
-        heroCount={heroCount}
-        setHeroCount={setHeroCount}
-      />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route
+          path="/hero"
+          element={
+            <>
+              <Background heroCount={heroCount} />
+              <Navbar />
+              <Hero
+                heroData={heroData[heroCount]}
+                heroCount={heroCount}
+                setHeroCount={setHeroCount}
+              />
+            </>
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
